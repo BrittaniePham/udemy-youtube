ddcca951ac09277f59bfebbe49bf8a886b1f6f1f
Brittanie Pham <brittaniepham@gmail.com>
2018-08-06 14:34:03 -0600

    constructors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import SearchBar from './components/searchBar'
+import YTSearch from 'youtube-api-search'
+const API_KEY = 'AIzaSyAf4zSrCvUMWHQHT0Q7e0u3uvUOvBxCiMk'
 
-const App = () => {
-  return(
-    <div>
-      Hi!
-    </div>
-  )
+class App extends Component {
+  
+  constructor(props) {
+    super(props)
+    
+    this.state = { videos: [] }
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (data) => {
+      this.setState({ videos: data })
+    })
+  }
+
+  render() {
+    return(
+      <div>
+        <SearchBar />
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(<App/>, document.querySelector('.container'))
