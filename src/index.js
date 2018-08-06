87975555b5503304ce81e031790976218661f976
Brittanie Pham <brittaniepham@gmail.com>
2018-08-06 16:43:55 -0600

    video detail/list/listItem display

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-import SearchBar from './components/searchBar'
 import YTSearch from 'youtube-api-search'
+import SearchBar from './components/searchBar'
+import VideoList from './components/VideoList'
+import VideoDetail from './components/VideoDetail'
 const API_KEY = 'AIzaSyAf4zSrCvUMWHQHT0Q7e0u3uvUOvBxCiMk'
 
 class App extends Component {
@@ -11,8 +13,8 @@ class App extends Component {
     
     this.state = { videos: [] }
 
-    YTSearch({key: API_KEY, term: 'surfboards'}, (data) => {
-      this.setState({ videos: data })
+    YTSearch({key: API_KEY, term: 'cats'}, (videos) => {
+      this.setState({ videos })
     })
   }
 
@@ -20,6 +22,8 @@ class App extends Component {
     return(
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.videos[0]}/>
+        <VideoList videos={this.state.videos}/>
       </div>
     )
   }
